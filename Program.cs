d8dbb5636261329e2f41dabc8012535348af94d1
joao archiolli <joao.roberto_@2001@hotmail.com>
2023-11-16 21:29:45 -0300

    final commit: swagger on

--- Program.cs
+++ Program.cs
@@ -29,6 +29,8 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 app.UseAuthentication();
